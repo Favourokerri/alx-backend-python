3cd5fd3134a04ea16c4943e3202b8742609610e0
Favourokerri <favourokerri767@gmail.com>
2023-12-07 22:42:45 +0000

    2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import math
-""" floor """
+""" type-annotated floor """
 
 
 def floor(n: float) -> int:
-    """ floor """
+    """ type-annotated floor """
     return math.floor(n)
